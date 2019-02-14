c27f40672e12922fc9d25c106005d43bb101b46a
wangfy263 <wfy339095626@gmail.com>
2019-02-14 13:37:48 +0800

    update

--- ES5promise.js
+++ ES5promise.js
@@ -121,13 +121,3 @@ function analysisPromise(promise, x, resolve, reject){
         reject(e);
     }
 }
-
-Promise.defer = Promise.deferred = function () {
-    let dfd = {}
-    dfd.promise = new Promise((resolve, reject) => {
-        dfd.resolve = resolve;
-        dfd.reject = reject;
-    });
-    return dfd;
-}
-module.exports = Promise;
